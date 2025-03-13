24f4d24c4db49d5b9ad1e523869bf511222062b4
Haykes <90206245+Haykes@users.noreply.github.com>
2025-03-13 23:31:49 +0100

    [Feat] tests : ajout tests unitaires fonctionnalité historique CalculatorService

--- src/Service/CalculatorService.php
+++ src/Service/CalculatorService.php
@@ -25,5 +25,25 @@ class CalculatorService
         return $result;
     }
 
+    public function multiply(float $a, float $b): float
+    {
+        $result = $a * $b;
+        $this->storeInHistory($result);
+        return $result;
+    }
 
+    public function getHistory(): array
+    {
+        return $this->history;
+    }
+
+    public function clearHistory(): void
+    {
+        $this->history = [];
+    }
+
+    private function storeInHistory(float $result): void
+    {
+        $this->history[] = $result;
+    }
 }
